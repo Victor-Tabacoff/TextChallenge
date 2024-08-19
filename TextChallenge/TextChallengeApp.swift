a65611b57d78b25efccc4688179834b884f6f182
Paul Hennig <paul.hennig@d214.org>
2024-08-19 08:57:46 -0500

    Initial Commit

---
+++ TextChallenge/TextChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextChallengeApp.swift
+//  TextChallenge
+//
+//  Created by Victor B. Tabacoff on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TextChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
